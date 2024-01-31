305ac5faf4692d0fbef652e7671a856bfeb83ee7
Carlo Fantozzi <carlo.fantozzi@unipd.it>
2024-01-31 16:15:57 +0100

    chore: Minor improvements in Chapter 18 Chiefly, implemented suggestions from Lint

--- Chapter 18/TemperatureConverter/app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ Chapter 18/TemperatureConverter/app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -3,23 +3,17 @@ package com.hfad.temperatureconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.material.Button
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.material.TextField
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
